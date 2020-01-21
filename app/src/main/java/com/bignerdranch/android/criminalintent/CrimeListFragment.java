ff62d55c8c7423766853f87998489ce0db29942d
Abe1008 <abe1008@gmail.com>
2020-01-22 00:11:58 +0300

    стр. 191 замена android.support.v7.widget.RecyclerView на androidx.recyclerview.widget.RecyclerView

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    // в новой Android Studio  нужно использовать (androidx)
+    // androidx.recyclerview.widget.RecyclerView
+    // https://stackoverflow.com/questions/25477860/error-inflating-class-android-support-v7-widget-recyclerview
+    // и добавить библиотеку androidx.recyclerview:1.1.0
+    // .
+}
